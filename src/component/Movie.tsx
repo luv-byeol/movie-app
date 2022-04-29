106c7f94b68e7da886215d01aa56c6b6ec145fd2
luv-byeol <luv_byeol@icloud.com>
2022-04-29 12:51:22 +0900

    styles update

--- src/component/Movie.tsx
+++ src/component/Movie.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { MovieType } from "interface/movie"
+import { MovieType } from "interface/movie";
 // import { Link } from "react-router-dom";
 
 function Movie() {
@@ -8,7 +8,7 @@ function Movie() {
     const getMovies = async () => {
         const json = await (
             await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
             )
         ).json();
         setMovies(json.data.movies);
@@ -17,18 +17,21 @@ function Movie() {
     useEffect(() => {
         getMovies();
     }, [])
+    console.log(movies)
 
     return (
-        <div>
-            {loading ? <h1>Loading...</h1> :
-                <div>
+        <div className="container">
+            {loading ? <div className="loader">
+          <span>Loading...</span>
+        </div> :
+                <div className="movies"> 
                     {movies.map((movie) => (
-                        <div key={movie.id}>
-                            <img src={movie.medium_cover_image} alt={movie.title} />
-                            <h2><a href={`${movie.id}`}>{movie.title}</a></h2>
+                        <div className="movie" key={movie.id}>
+                            <img className="movie__img" src={movie.medium_cover_image} alt={movie.title} />
+                            <h2 className="movie__title"><a href={`${movie.id}`}>{movie.title}</a></h2>
                             {/* <h2><Link to={`${movie.id}`}>{movie.title}</Link></h2> */}
-                            <p>{movie.summary}</p>
-                            <ul>
+                            <h3 className="movie__year">{movie.year}</h3>
+                            <ul className="movie__genres">
                                 {movie.genres.map((g) => (
                                     <li key={g}>{g}</li>
                                 ))}
--- src/interface/movie.ts
+++ src/interface/movie.ts
@@ -4,4 +4,5 @@ export interface MovieType {
     title: string;
     summary: string;
     genres: string[];
+    year: number;
 }
